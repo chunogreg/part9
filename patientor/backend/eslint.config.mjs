d0dc9410960eefcebc89a1952c5ebd4089028141
chunogreg <chunogreg@yahoo.com>
2026-09-20 13:02:43 +0100

    Fix typos in tsconfig

--- patientor/backend/eslint.config.mjs
+++ patientor/backend/eslint.config.mjs
@@ -19,12 +19,11 @@ export default tseslint.config({
   rules: {
     "@stylistic/semi": "error",
     "@typescript-eslint/no-unsafe-assignment": "error",
-    "@typescript-eslint/no-implicit-any": "error",
+    "@typescript-eslint/no-explicit-any": "error",
     "@typescript-eslint/explicit-function-return-type": "off",
     "@typescript-eslint/explicit-module-boundary-types": "off",
     "@typescript-eslint/restrict-template-expressions": "off",
     "@typescript-eslint/restrict-plus-operands": "off",
-    "@typescript-eslint/consistent-type-imports": "error",
     "@typescript-eslint/no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
   },
 });
